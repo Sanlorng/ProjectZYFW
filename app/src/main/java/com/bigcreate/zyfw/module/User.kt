1ee4b0be2082c4203482693c64c6b19b6eeedceb
sanlorng <fujia625@gmail.com>
2018-04-10 15:25:44 +0800

    初始化项目

--- app/src/main/java/com/bigcreate/zyfw/module/User.kt
+++ app/src/main/java/com/bigcreate/zyfw/module/User.kt
@@ -6,5 +6,5 @@ import org.litepal.crud.DataSupport
  * Create by Sanlorng on 2018/4/9
  */
 data class User(val name: String, val  password: String): DataSupport(){
-
+    var isLoginUser : Int ?= null
 }
